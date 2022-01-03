0920ffeb51d1a113c56534c0024573955b65d052
crespire <li.simmon@gmail.com>
2022-01-02 23:35:36 -0500

    Add horizontal tests for king

--- spec/movement_spec.rb
+++ spec/movement_spec.rb
@@ -25,9 +25,10 @@ describe Movement do
 
   context '#horizontal_move' do
     let(:board) { Board.new }
-    subject(:rook_test) { described_class.new(board) }
 
     context 'with a Rook' do
+      subject(:rook_test) { described_class.new(board) }
+
       context 'on an empty board' do
         it 'when Rook is on a8, provides the correct list of movement options' do
           board.make_board('r7/8/8/8/8/8/8/8 b - - 1 2')
@@ -41,24 +42,59 @@ describe Movement do
           expect(rook_test.horizontal_move(cell)).to eq(%w[a4 b4 c4 e4 f4 g4 h4])
         end
       end
+
+      context 'where there is a friendly piece on the path' do
+        xit 'when the Rook starts on a8, returns the correct list of movement options' do 
+        end
+  
+        xit 'when the Rook is on d4, provides the correct list of movement options' do
+        end
+      end
+  
+      context 'where there is an enemy piece on the path' do
+        xit 'when the Rook is on a8, returns the correct list of movement options including a capture' do
+        end
+  
+        xit 'when the Rook is on d4, provides the correct list of movement options' do
+        end
+      end
+    end
+
+  context 'with a King' do
+    subject(:king_test) { described_class.new(board) }
+
+    context 'on an empty board' do
+      it 'when King is on a8, provides the correct list of movement options' do
+        board.make_board('k7/8/8/8/8/8/8/8 b - - 1 2')
+        cell = board.cell('a8')
+        expect(king_test.horizontal_move(cell)).to eq(%w[b8])
+      end
+
+      it 'when the King is on d4, provides the correct list of movement options' do
+        board.make_board('8/8/8/8/3k4/8/8/8 b - - 1 2')
+        cell = board.cell('d4')
+        expect(king_test.horizontal_move(cell)).to eq(%w[c4 e4])
+      end
     end
 
     context 'where there is a friendly piece on the path' do
-      xit 'when the Rook starts on a8, returns the correct list of movement options' do 
+      xit 'when the King starts on a8, returns the correct list of movement options' do 
       end
 
-      xit 'when the Rook is on d4, provides the correct list of movement options' do
+      xit 'when the King is on d4, provides the correct list of movement options' do
       end
     end
 
     context 'where there is an enemy piece on the path' do
-      xit 'when the Rook is on a8, returns the correct list of movement options including a capture' do
+      xit 'when the King is on a8, returns the correct list of movement options including a capture' do
       end
 
       xit 'when the Rook is on d4, provides the correct list of movement options' do
       end
     end
   end
+
+end
 
   context '#valid_moves' do
     context 'when provided a Cell with a Rook' do
