2f8acbb84927311b7af5cc322fb425afdae17034
crespire <li.simmon@gmail.com>
2022-01-11 14:13:51 -0500

    Define methods in Display

--- lib/display.rb
+++ lib/display.rb
@@ -6,4 +6,21 @@ class Display
   def initialize(board = nil)
     @board = board
   end
-end
+
+  def show_board(board = @board)
+    display_track = 0
+    board.each do |rank|
+      rank.each do |cell|
+        puts colorize_cell(cell.to_fen, display_track.ood?)
+        display_track += 1
+      end
+      display_track += 1
+    end
+  end
+
+  private
+
+  def colorize_cell(text, flip = false)
+    flip ? "\e[7m#{text}\e27m" : text
+  end
+end 
