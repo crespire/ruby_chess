4e1755f60341e571cf75899f07f1c825a5beba0d
crespire <li.simmon@gmail.com>
2022-01-17 16:19:29 -0500

    Update tests on Checkmate

--- spec/checkmate_spec.rb
+++ spec/checkmate_spec.rb
@@ -46,62 +46,96 @@ describe Checkmate do
   end
 
   context 'when provided a board with black in a checkmate situation' do
-    subject(:checkmate) { described_class.new(board) }
+    subject(:checkmate1) { described_class.new(board) }
 
     before do
       board.make_board('4k3/1b2P3/4KN2/8/8/8/7p/8 b - - 1 1')
     end
 
     it 'returns true for checkmate' do
-      expect(checkmate.checkmate?).to be true
+      expect(checkmate1.checkmate?).to be true
     end
 
     it 'returns true for check' do
-      expect(checkmate.check?).to be true
+      expect(checkmate1.check?).to be true
     end
 
-    it 'returns true for stalemate' do
-      expect(checkmate.stalemate?).to be false
+    it 'returns false for stalemate' do
+      expect(checkmate1.stalemate?).to be false
     end
   end
 
   context 'when provided as board with white in a checkmate situation' do
-    subject(:checkmate) { described_class.new(board) }
+    subject(:checkmate2) { described_class.new(board) }
 
     before do
       board.make_board('rnb1kbnr/pppp1ppp/8/4p3/6Pq/5P2/PPPPP2P/RNBQKBNR w KQkq - 1 3')
     end
 
     it 'returns true for checkmate' do
-      expect(checkmate.checkmate?).to be true
+      expect(checkmate2.checkmate?).to be true
     end
 
     it 'returns true for check' do
-      expect(checkmate.check?).to be true
+      expect(checkmate2.check?).to be true
     end
 
-    it 'returns true for stalemate' do
-      expect(checkmate.stalemate?).to be false
+    it 'returns false for stalemate' do
+      expect(checkmate2.stalemate?).to be false
     end
   end
 
   context 'when provided a board with white in a checkmate situation by a knight' do
-    subject(:checkmate) { described_class.new(board) }
+    subject(:checkmate3) { described_class.new(board) }
 
     before do
       board.make_board('r1b1k2r/ppppqppp/2n5/8/1PP2B2/3n1N2/1P1NPPPP/R2QKB1R w KQkq - 1 9')
     end
 
     it 'returns true for checkmate' do
-      expect(checkmate.checkmate?).to be true
+      expect(checkmate3.checkmate?).to be true
     end
 
     it 'returns true for check' do
-      expect(checkmate.check?).to be true
+      expect(checkmate3.check?).to be true
     end
 
-    it 'returns true for stalemate' do
-      expect(checkmate.stalemate?).to be false
+    it 'returns false for stalemate' do
+      expect(checkmate3.stalemate?).to be false
+    end
+  end
+
+  context 'when provided a board with black in a checkmate situation by a bishop' do
+    subject(:checkmate4) { described_class.new(board) }
+
+    before do
+      board.make_board('r1bqkbnr/pp1ppBpp/8/2p5/1n6/5R2/PPPPPPPP/RNBQK1N1 b Qkq - 0 4')
+    end
+
+    it 'returns true for checkmate' do
+      expect(checkmate4.checkmate?).to be true
+    end
+
+    it 'returns true for check' do
+      expect(checkmate4.check?).to be true
+    end
+
+    it 'returns false for stalemate' do
+      expect(checkmate4.stalemate?).to be false
+    end
+  end
+
+  context 'when provided a board with black in a checkmate situation by a pawn' do
+    subject(:checkmate5) { described_class.new(board) }
+
+    before do
+      board.make_board('r1bqkbnr/pp1ppPpp/8/2p5/1n6/5R2/PPPPPPP1/RNBQKBN1 b Qkq - 0 1')
+    end
+
+    it 'returns the correct mate status' do
+      expect(checkmate5.checkmate?).to be true
+      expect(checkmate5.check?).to be true
+      expect(checkmate5.stalemate?).to be false
     end
   end
 end
