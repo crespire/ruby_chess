88111b60933b7a92fdf5da97679549e8744f5606
crespire <li.simmon@gmail.com>
2022-01-21 17:28:00 -0500

    Update Knight class to use var

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -7,7 +7,7 @@ class Knight < Piece
     moves = []
     offsets = [[2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2], [1, 2]] #[file, rank] offset pairs
     offsets.each do |offset|
-      moves << Move.new(board, 'b8', offset)
+      moves << Move.new(board, origin, offset)
     end
     moves
   end
