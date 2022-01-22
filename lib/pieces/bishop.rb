47a1163f9a09c7575be89af0e30e9ab4b0bc4ded
crespire <li.simmon@gmail.com>
2022-01-22 08:40:47 -0500

    Add Bishop moves

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -3,7 +3,14 @@
 # lib/pieces/bishop.rb
 
 class Bishop < Piece
-  def moves(board = nil, origin = nil)
+  def moves(board, origin)
+    moves = []
+    # Clockwise from north @ 12
+    offsets = [[1, 1], [1, -0], [-1, -1], [-1, 1]]
+    offsets.each do |offset|
+      moves << Move.new(board, origin, offset, 7)
+    end
+    moves
   end
 
   def slides?
