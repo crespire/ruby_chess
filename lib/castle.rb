d054e27afce3a6278bbc9258de410dd99c868ebe
crespire <li.simmon@gmail.com>
2022-01-28 15:30:19 -0500

    Add method to update rights

--- lib/castle.rb
+++ lib/castle.rb
@@ -15,4 +15,20 @@ class Castle
     @game = game
     @board = game.board
   end
+
+  def update_rights(game, piece, cell)
+    return unless piece.is_a?(King) || piece.is_a?(Rook)
+  
+    rights = game.castle.dup
+    delete_rights = 'KQ'
+    delete_rights.downcase if piece.black?
+    if piece.is_a?(Rook)
+      king_side = piece.white? ? cell.name > 'e' : cell.name < 'e'
+      king_side ? rights.delete!(delete_rights[0]) : rights.delete!(delete_rights[1])
+    else
+      rights.delete!(delete_rights)
+    end
+    rights = '-' if rights.empty?
+    game.castle = rights
+  end
 end
